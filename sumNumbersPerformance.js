75549f24e16521d5062e8bb1e54e64de06bbbf30
Herob527 <61355107+Herob527@users.noreply.github.com>
2021-01-26 20:10:25 +0100

    Updated sumNumbersPerformance.js

--- sumNumbersPerformance.js
+++ sumNumbersPerformance.js
@@ -1,40 +1,36 @@
 import benchmark from "benchmark"
 import { sendBenchmarkResultsToApiServer } from "./sendBenchmarkResultsToApiServer.js";
-import { initializeBenchmark } from "./initializeBenchmark.js";
+import { initForWasm, initializeBenchmark } from "./initializeBenchmark.js";
 
-const benchmarkValues = new initializeBenchmark(2 ** 10);
-
-benchmarkValues._map((el, index) => index);
-
-const chunkAmount = 32;
-const chunkedArray = benchmarkValues.chunkArray(chunkAmount);
+const testsAmount = 32;
 
 
-for (let counter = 0; counter < chunkAmount; counter++) {
+for (let sizeMultiplier = 1; sizeMultiplier <= testsAmount; sizeMultiplier++) {
 	const suite = new benchmark.Suite;
-	let arrayForBenchmark = [];
-	for (let pushIndex = 0; pushIndex <= counter; pushIndex++) {
-		arrayForBenchmark.push(chunkedArray[pushIndex]);
-	}
-	arrayForBenchmark = arrayForBenchmark.flat(Infinity);
-	const WASMUInt32Array = new Uint32Array(arrayForBenchmark);
-	const length = arrayForBenchmark.length;
+	const len = 64 * sizeMultiplier;
 
-	console.log(`\nBenchmarking for ${length} values. ${chunkAmount - counter - 1} benchamrks left`);
+	const arrayForBenchmark = new initializeBenchmark(len);
+	const wasmForBenchmark = new initForWasm('u32', len);
+	const incrementingFunction = (el, index) => index;
+
+	arrayForBenchmark._map(incrementingFunction);
+	wasmForBenchmark._map(incrementingFunction)
+
+	console.log(`\nBenchmarking for ${len} values. ${testsAmount - sizeMultiplier} benchmarks left to start`);
 
 	const sumReduce = () => arrayForBenchmark.reduce((acc, cur) => acc + cur, 0);
-	const sumWASMReduce = () => WASMUInt32Array.reduce((acc, cur) => acc + cur, 0);
+	const sumWASMReduce = () => wasmForBenchmark.reduce((acc, cur) => acc + cur, 0);
 	const sumFor = () => {
 		let sum = 0;
-		for (let sumIt = 0; sumIt < length; sumIt++) {
+		for (let sumIt = 0; sumIt < len; sumIt++) {
 			sum += arrayForBenchmark[sumIt];
 		}
 		return sum;
 	}
 	const sumWASMFor = () => {
 		let sum = 0;
-		for (let sumIt = 0; sumIt < length; sumIt++) {
-			sum += WASMUInt32Array[sumIt];
+		for (let sumIt = 0; sumIt < len; sumIt++) {
+			sum += wasmForBenchmark[sumIt];
 		}
 		return sum;
 	}
@@ -47,22 +43,22 @@ for (let counter = 0; counter < chunkAmount; counter++) {
 	}
 	const sumWASMForOf = () => {
 		let sum = 0;
-		for (let val of WASMUInt32Array) {
+		for (let val of wasmForBenchmark) {
 			sum += val;
 		}
 		return sum;
 	}
 	const sumRecursive = (acc = 0, cur = arrayForBenchmark[0], index = 0) => {
-		if (index >= length) {
+		if (index >= len) {
 			return acc;
 		}
 		return sumRecursive(acc + cur, arrayForBenchmark[index + 1], ++index);
 	}
-	const sumWASMRecursive = (acc = 0, cur = WASMUInt32Array[0], index = 0) => {
-		if (index >= length) {
+	const sumWASMRecursive = (acc = 0, cur = wasmForBenchmark[0], index = 0) => {
+		if (index >= len) {
 			return acc;
 		}
-		return sumWASMRecursive(acc + cur, WASMUInt32Array[index + 1], ++index);
+		return sumWASMRecursive(acc + cur, wasmForBenchmark[index + 1], ++index);
 	}
 	suite
 		.add('Array sum reduce', sumReduce)
@@ -73,9 +69,9 @@ for (let counter = 0; counter < chunkAmount; counter++) {
 		.add('Uint32 sum for', sumWASMFor)
 		.add('Uint32 sum for of', sumWASMForOf)
 		.add('Uint32 sum recursive', sumWASMRecursive)
-		.on('cycle', event => console.log(`\t${length}: ${String(event.target)}`))
+		.on('cycle', event => console.log(`\t${len}: ${String(event.target)}`))
 		.on('complete', (event) => {
-			const connect = new sendBenchmarkResultsToApiServer(event, 'array_size', length)
+			const connect = new sendBenchmarkResultsToApiServer(event, 'array_size', len)
 			connect._send("sum_numbers");
 		})
 		.run({'async': true})
